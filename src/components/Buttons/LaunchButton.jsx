9cc94b5ef73e1c62666733704c91dfd27bc22427
VndesTokenVDE <166009683+VndesTokenVDE@users.noreply.github.com>
2024-04-25 16:39:38 +0700

    Update LaunchButton.jsx

--- src/components/Buttons/LaunchButton.jsx
+++ src/components/Buttons/LaunchButton.jsx
@@ -4,7 +4,7 @@ import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 const LaunchButton = ({ sx = {}, ...props }) => {
   return (
-    <Button target="_blank" href="https://app.uniswap.org/swap" arrow fit variant="contained" sx={{ borderRadius: 4, ...sx }} {...props}>
+    <Button target="_blank" href="https://pancakeswap.finance/swap?outputCurrency=0xcB3E4688C4cd90022F74f5C70A775172eCaB4D52&chainId=56" arrow fit variant="contained" sx={{ borderRadius: 4, ...sx }} {...props}>
      BUY NOW
     </Button>
   );
