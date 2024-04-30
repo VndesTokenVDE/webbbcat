860b505ebb600bdc44e998bf96211f41f1f3c4dd
VndesTokenVDE <166009683+VndesTokenVDE@users.noreply.github.com>
2024-04-30 11:15:11 +0700

    Update Section3.jsx

--- src/containers/Section3.jsx
+++ src/containers/Section3.jsx
@@ -2,9 +2,10 @@ import { Container, Grid, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import React from "react";
 import Title from "../components/Title";
-import { section3Content } from "../utils/content";
+import { section3Link,section3Content } from "../utils/content";
 
 const { title, ITEMS } = section3Content;
+const { title1, LIST } = section3Link;
 
 const Section3 = () => {
   return (
@@ -14,7 +15,21 @@ const Section3 = () => {
           {title}
         </Title>
       </Container>
-
+      
+      <Grid container spacing={3} sx={{ mt: 4 }} justifyContent="center" >
+        {LIST.map(({ name, logo,link }) => (
+          <Grid item xs={6} md={3} lg={3} key={name}>
+            <Stack alignItems="center">
+              <a target={'_blank'} href={link} ><img
+                src={logo}
+                style={{ width: "100%", objectFit: "contain" }}
+              />
+              </a>
+              
+            </Stack>
+          </Grid>
+        ))}
+      </Grid>
       <Grid container spacing={5} sx={{ mt: 4 }} justifyContent="center">
         {ITEMS.map(({ name, logo }) => (
           <Grid item xs={4} md={3} lg={2.4} key={name}>
